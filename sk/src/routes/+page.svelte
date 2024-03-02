ac779abcfe9e9176fe177d1ffae1cee369f7826a
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-03-02 14:16:58 +0100

    feat: set 10 cols and text hidden

--- sk/src/routes/+page.svelte
+++ sk/src/routes/+page.svelte
@@ -18,44 +18,35 @@
 
     let items = data.items;
 
-    let gapX = 10;
-    let gapY = 10;
-
-    const breakpoint = 1200
-    const column = 6
-
-    const itemSize = { height: 40 };
+    const itemSize = {
+        height: 35
+    };
 </script>
 
 <div class=demo-container>
-    <!-- <Grid bind:items={items} gap={[gapX, gapY]} rowHeight={100} let:item {cols}>
-        <button class="demo-widget" on:click={() => {modalStore.trigger(modal);}}>
-            <h1>
-                {item.text}
-            </h1>
-        </button>
-    </Grid>  -->
-    <Grid {itemSize} cols={6} collision="none">
+    <Grid {itemSize} cols={10} collision="none">
         {#each items as item}
-            <GridItem x={item.x} y={item.y} w={item.w} h={item.h}>
-                <div class="item">{item.text}</div>
+            <GridItem x={item.x} y={item.y} w={item.w} h={item.h} class="grid-item">
+                <button class="item btn-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" on:click={() => {modalStore.trigger(modal)}}>
+                    <p class="text-xl overflow-hidden">
+                        {item.text}
+                    </p>
+                </button>
             </GridItem>
         {/each}
     </Grid>
 </div>
 
 <style>
-    :global(.svlt-grid-shadow) {
-        /* Back shadow */
-        background: rgb(47, 0, 255);
+    .grid-item {
+        /* display: flex;
+        justify-content: center;
+        align-items: center; */
     }
-    :global(.svlt-grid-resizer::after) {
-        /* Resizer color */
-        border-color: rgb(18, 62, 255) !important;
-    }
-    :global(.svlt-grid-item) {
-        /* Resizer color */
-        background: rgb(98, 194, 165) !important;
+
+    .item {
+        height: 100%;
+        width: 100%;
     }
 
     .demo-widget {
