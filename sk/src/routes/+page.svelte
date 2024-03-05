09c1c505d9fc5bae676fac88c9a605321ee5d58a
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-03-05 22:18:16 +0100

    feat: set round button

--- sk/src/routes/+page.svelte
+++ sk/src/routes/+page.svelte
@@ -65,7 +65,7 @@
     <Grid {itemSize} cols={data.grid_max_col} collision="none">
         {#each items as item}
             <GridItem x={item.x} y={item.y} w={item.w} h={item.h} resizable={true} movable={item.movable} class="grid-item">
-                <button class="item btn-lg  bg-gradient-to-r px-1 from-indigo-500 via-purple-500 to-pink-500" on:click={() => handleModal(item)}>
+                <button class="btn item bg-gradient-to-r px-1 from-indigo-500 via-purple-500 to-pink-500" on:click={() => handleModal(item)}>
                     <div class="h-full flex flex-col justify-around overflow-hidden">
                         <div class="text-xl overflow-hidden">
                             {item.text}
