6f1eb8d855cc595314c44f160295533ba9fde4a5
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-03-09 22:24:59 +0100

    fix: properly set pocketbase url from variable

--- sk/src/hooks.server.ts
+++ sk/src/hooks.server.ts
@@ -14,24 +14,24 @@ const grafana_url = fs.existsSync("/.dockerenv")
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    event.locals.pb = new PocketBase('http://localhost:8090');
+  event.locals.pb = new PocketBase(pocketbase_url);
+  
+  // load the store data from the request cookie string
+  event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
-    // load the store data from the request cookie string
-    event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+  try {
+      // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+      event.locals.pb.authStore.isValid && await event.locals.pb.collection('users').authRefresh();
+  } catch (_) {
+      // clear the auth store on failed refresh
+      event.locals.pb.authStore.clear();
+  }
 
-    try {
-        // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
-        event.locals.pb.authStore.isValid && await event.locals.pb.collection('users').authRefresh();
-    } catch (_) {
-        // clear the auth store on failed refresh
-        event.locals.pb.authStore.clear();
-    }
+  const response = await resolve(event);
+  event.locals.user = event.locals.pb.authStore.model
 
-    const response = await resolve(event);
-    event.locals.user = event.locals.pb.authStore.model
+  // send back the default 'pb_auth' cookie to the client with the latest store state
+  response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie());
 
-    // send back the default 'pb_auth' cookie to the client with the latest store state
-    response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie());
-
-    return response;
+  return response;
 }
