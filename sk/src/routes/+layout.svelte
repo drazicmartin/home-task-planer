a519613a8cc9d588d97e0e4fbf0003f982c0ee2c
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-03-19 21:41:53 +0100

    feat: redirect to grafana

--- sk/src/routes/+layout.svelte
+++ sk/src/routes/+layout.svelte
@@ -30,7 +30,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                 </svg>
             </li>
-            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="/dashboard">Dashboard</a></li>
+            <li><a class="text-sm text-gray-400 hover:text-gray-500" href="/grafana">Grafana</a></li>
         </ul>
     </nav>
 </header>
