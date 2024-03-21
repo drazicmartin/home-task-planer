438e54885844c2efa2dbb9f996e4f9da3e581d2b
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-03-21 20:45:07 +0100

    fix: ux style on mobile

--- sk/src/routes/history/+page.svelte
+++ sk/src/routes/history/+page.svelte
@@ -4,31 +4,29 @@
     let items = data.items;
 </script>
 
-<div class="background justify-center snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10">
-    <ul>
-        {#each items as item}
-        <li class="pb-1 p-4">
-           <div class="flex items-center p-3 space-x-4 rtl:space-x-reverse card variant-glass-success capitalize">
-              <div class="flex-1 min-w-0">
-                 <p class="text-xl font-medium text-emerald-950 truncate">
-                    {item.expand?.task?.name ?? "TASK_NAME"}
-                 </p>
-                 <div class="inline-flex items-center px-2 text-xl font-semibold text-gray-50">
-                     <p class="text-sm text-gray-950 truncate">
-                        - {item.expand?.user?.name ?? "USER_NAME"}
-                     </p>
-                     <p class="text-sm px-5 text-gray-800 truncate">
-                        - {new Date(item.created).toLocaleString() ?? "DATE"} -
-                     </p>
-                </div>
-              </div>
-               <button class="btn-icon btn-icon-sm variant-ghost-surface items-center px-2 text-xl font-semibold text-emerald-950">
-                  {item.score}  
-               </button>
-           </div>
-        </li>
-        {/each}
-     </ul>
+<div class="background snap-x scroll-px-4 snap-mandatory scroll-smooth gap-1 overflow-x-auto px-4">
+   {#each items as item}
+      <div class="my-3">
+         <div class="flex items-center p-3 space-x-4 rtl:space-x-reverse card variant-glass-success capitalize">
+            <div class="flex-1 min-w-0">
+               <p class="text-xl font-medium text-emerald-950 truncate">
+                  {item.expand?.task?.name ?? "TASK_NAME"}
+               </p>
+               <div class="inline-flex items-center px-2 text-xl font-semibold text-gray-50">
+                  <p class="text-sm text-gray-950 truncate">
+                     - {item.expand?.user?.name ?? "USER_NAME"}
+                  </p>
+                  <p class="text-sm px-5 text-gray-800 truncate">
+                     - {new Date(item.created).toLocaleString() ?? "DATE"} -
+                  </p>
+               </div>
+            </div>
+            <button class="btn-icon btn-icon-sm variant-ghost-surface items-center px-2 text-xl font-semibold text-emerald-950">
+               {item.score}  
+            </button>
+         </div>
+      </div>
+   {/each}
 </div>
 
 <style>
